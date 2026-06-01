9c14c5486b63487886e712dfbd13e62301726d2b
iarribanav <iarribanav@educaion.navarra.es>
2026-06-01 17:36:09 +0200

    nueva ruta 3

--- src/main/java/es/masanz/ut7/pokemonfx/manager/MapManager.java
+++ src/main/java/es/masanz/ut7/pokemonfx/manager/MapManager.java
@@ -2,6 +2,7 @@ package es.masanz.ut7.pokemonfx.manager;
 
 import es.masanz.ut7.pokemonfx.model.base.Evento;
 import es.masanz.ut7.pokemonfx.model.base.Mapa;
+import es.masanz.ut7.pokemonfx.model.map.Ruta3;
 import es.masanz.ut7.pokemonfx.model.pokemons.Bulbasaur;
 import es.masanz.ut7.pokemonfx.model.base.Entrenador;
 import es.masanz.ut7.pokemonfx.model.enums.CollisionType;
@@ -12,6 +13,7 @@ import es.masanz.ut7.pokemonfx.model.map.Ruta2;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import static es.masanz.ut7.pokemonfx.util.Configuration.TILE_SIZE;
@@ -31,13 +33,16 @@ public class MapManager {
     public static void initMapas(){
         Mapa ruta1 = new Ruta1();
         Mapa ruta2 = new Ruta2();
+        Mapa ruta3 = new Ruta3();
         mapas = new HashMap<>();
         mapas.put(ruta1.getNombre(), ruta1);
         mapas.put(ruta2.getNombre(), ruta2);
+        mapas.put(ruta3.getNombre(), ruta3);
 
 //        rutaSeleccionada = ruta1.getNombre();
 //        cargarRuta("Ruta 1");
-        cargarRuta("Ruta 2");
+//        cargarRuta("Ruta 2");
+        cargarRuta("Ruta 3");
 
     }
 
